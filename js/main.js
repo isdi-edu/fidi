4cda58e9f401f19bb3f06d10cf3c697df1f3f8cd
Ruben Bernardez <rubenbp@gmail.com>
2015-03-24 13:40:38 +0100

    enlaces menú superior

--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@ $(function() {
     event.preventDefault();
     $.scrollTo("body", 1000);
   });
+
+  $(".js-scroll").on("click", function(event) {
+    event.preventDefault();
+    var duration = parseInt($(this).data("duration"));
+    $.scrollTo("a[name='" + $(this).attr("href").substring(1) + "']", duration);
+  });
 });
