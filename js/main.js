0f844451d037d76b46f78b03281d5f53140f8371
Ruben Bernardez <rubenbp@gmail.com>
2015-04-13 17:09:34 +0200

    añadiendo política de cookies

--- js/main.js
+++ js/main.js
@@ -13,4 +13,21 @@ $(function() {
   $('[data-toggle="tooltip"]').tooltip({
     trigger: 'hover click'
   });
+
+  var lang = navigator.language || navigator.userLanguage;
+  var options;
+  if (lang.toLowerCase().startsWith("es")) {
+    options = {
+      message: "Utilizamos cookies para conocer cómo te mueves por la web. Si aceptas o continuas navegando, entendemos que estás de acuerdo con su uso.",
+      acceptText: "Acepto",
+      policyText: "Política de cookies"
+    }
+  } else {
+    options = {
+      message: "We use cookies to understand how you move through the web. If you accept or continuous browsing, we understand that you agree with its use.",
+      acceptText: "Accept",
+      policyText: "Cookies policy"
+    }
+  }
+  $.cookieBar(options);
 });
